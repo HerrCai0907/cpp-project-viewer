eae4434e4c408dbefb4714ace86aef3a0fdf5a71
Congcong Cai <congcongcai0907@163.com>
2024-05-12 11:15:21 +0800

    refactor

---
+++ include/cpjview/server/persistence/string_pool.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <set>
+namespace cpjview::persistence {
+
+class StringPool {
+  using StringCacheType = std::set<std::string>;
+  StringCacheType m_string_cache{};
+
+public:
+  const char *ensure_string_in_cache(std::string_view str) {
+    StringCacheType::const_iterator it =
+        m_string_cache.insert(std::string{str}).first;
+    return it->c_str();
+  }
+};
+
+} // namespace cpjview::persistence
--- src/server/persistence/storage.cpp
+++ src/server/persistence/storage.cpp
@@ -2,21 +2,14 @@
 #include "cpjview/server/persistence/error_code.hpp"
 #include "cpjview/server/persistence/kv.hpp"
 #include "cpjview/server/persistence/project.hpp"
-#include <set>
+#include "cpjview/server/persistence/string_pool.hpp"
 #include <string>
 
 namespace cpjview::persistence {
 
 struct StorageImpl {
-  using StringCacheType = std::set<const std::string>;
-  StringCacheType m_string_cache{};
+  StringPool m_string_cache{};
   Project m_projects{};
-
-  const char *ensure_string_in_cache(std::string_view str) {
-    StringCacheType::const_iterator it =
-        m_string_cache.insert(std::string{str}).first;
-    return it->c_str();
-  }
 };
 
 Storage::Storage() { m_impl = new StorageImpl(); }
@@ -28,7 +21,7 @@ Storage::~Storage() { delete m_impl; }
 // ================================================================
 
 ErrorCodeResult Storage::put_project(std::string const &name) {
-  const char *str = m_impl->ensure_string_in_cache(name);
+  const char *str = m_impl->m_string_cache.ensure_string_in_cache(name);
   return m_impl->m_projects.force_create(str);
 }
 
@@ -47,9 +40,11 @@ std::vector<const char *> Storage::get_projects() const {
 void Storage::add_inheritance(std::string const &project_name,
                               std::string const &derived,
                               std::string const &base) {
-  const char *project_name_c_str = m_impl->ensure_string_in_cache(project_name);
-  const char *base_c_str = m_impl->ensure_string_in_cache(base);
-  const char *derived_c_str = m_impl->ensure_string_in_cache(derived);
+  const char *project_name_c_str =
+      m_impl->m_string_cache.ensure_string_in_cache(project_name);
+  const char *base_c_str = m_impl->m_string_cache.ensure_string_in_cache(base);
+  const char *derived_c_str =
+      m_impl->m_string_cache.ensure_string_in_cache(derived);
   m_impl->m_projects.modify_info(
       project_name_c_str,
       [base_c_str, derived_c_str](Project::Info &info) -> ErrorCodeResult {
@@ -61,7 +56,8 @@ void Storage::add_inheritance(std::string const &project_name,
 Result<std::vector<Storage::InheritancePair>, ErrorCode>
 Storage::get_all_inheritance(std::string const &project_name) const {
   using RetType = Result<std::vector<Storage::InheritancePair>, ErrorCode>;
-  const char *project_name_c_str = m_impl->ensure_string_in_cache(project_name);
+  const char *project_name_c_str =
+      m_impl->m_string_cache.ensure_string_in_cache(project_name);
   Project::Info *info = m_impl->m_projects.get_info(project_name_c_str);
   if (info == nullptr) {
     return RetType::failed(ErrorCode::not_found());
