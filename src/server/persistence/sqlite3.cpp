1684285e49cf6714342453a5a32eef432dc70472
Congcong Cai <congcong.cai@bmw.com>
2024-08-22 21:33:52 +0800

    wipg

--- src/server/persistence/sqlite3.cpp
+++ src/server/persistence/sqlite3.cpp
@@ -1,8 +1,28 @@
 
 #include "sqlite3.hpp"
 #include "spdlog/spdlog.h"
+#include "sqlite3.h"
+#include <memory>
 
 namespace cpjview::persistence::sqlite3 {
+
+void Sqlite3::DbDeleter::operator()(struct sqlite3 *ptr) const {
+  for (;;) {
+    int ret = sqlite3_close(ptr);
+    if (ret == SQLITE_OK) {
+      break;
+    }
+    if (ret == SQLITE_BUSY) {
+      spdlog::warn("[sqlite] close sqlite with unfinished operation");
+      continue;
+    }
+    spdlog::error("[sqlite] sqlite error {} when closing", sqlite3_errmsg(ptr));
+  }
+}
+
+void Sqlite3::ResponseDeleter::operator()(Sqlite3::Response *response) const {
+  sqlite3_free_table(response->m_message);
+}
 
 Result<void, const char *> Sqlite3::connect() {
   using RT = Result<void, const char *>;
@@ -22,24 +42,22 @@ Result<void, const char *> Sqlite3::connect() {
   return RT::success();
 }
 
-void Sqlite3::exec(std::string const &command) {
-  char **az_result;
-  int n_row = 0;
-  int n_column = 0;
-  char *z_err_msg;
-  int ret = sqlite3_get_table(m_db.get(), command.c_str(), &az_result, &n_row,
-                              &n_column, &z_err_msg);
-
-  if (ret != SQLITE_OK) {
-    spdlog::trace("[sqlite] z_err_msg {}", z_err_msg);
-    return;
+Sqlite3::ResponseUniquePtr Sqlite3::exec(std::string const &command) {
+  char *err_msg;
+  ResponseUniquePtr ret{new Response{}};
+  int ret_code = sqlite3_get_table(m_db.get(), command.c_str(), &ret->m_message,
+                                   &ret->m_row, &ret->m_column, &err_msg);
+  if (ret_code != SQLITE_OK) {
+    spdlog::trace("[sqlite] exec {} failed, error msg {}", command, err_msg);
+    return nullptr;
   }
-  spdlog::trace("[sqlite] row {} column {}", n_row, n_column);
-  for (int i = 0; i < n_column * n_row; i++) {
-    spdlog::trace("[sqlite] az_result[{}] {}", i, az_result[i]);
+  for (int i = 0; i < ret->m_row; i++) {
+    for (int j = 0; j < ret->m_column; i++) {
+      spdlog::trace("[sqlite] result[{},{}]: {}", i, j,
+                    ret->m_message[i * ret->m_column + j]);
+    }
   }
-
-  sqlite3_free_table(az_result);
+  return ret;
 }
 
 } // namespace cpjview::persistence::sqlite3
